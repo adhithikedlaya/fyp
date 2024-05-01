PyTorch model gradients fixed

Euler_1stOrderForward.py
@@ -1,24 +1,18 @@
-"""
-First-order forward Euler method
-
-Plot all variables VS time by numerically solving the ODEs
-"""
-
-import numpy as np
+import torch
 import matplotlib.pyplot as plt
 from scipy.fftpack import rfftfreq
 import Stimulus_PDCM_BOLD as spb
 import PlotFunctions as pf
 from NoiseTimeSeries import generate_pink_noise
+import math
 
 indicator = True  # True for CBF-CBV coupling, False for uncoupling
 plot_all = True  # plot all variables together
 
-
 # Reproduce Fig. 3
 stimulus = 1  # stimulus duration (unit: sec)
 func = spb.StimulusPDCMBOLD(w=stimulus, mu=1, lamb=0.2,
-                            c=1)  # solid thick line
+                            c=1)  # solid thick line - TMTT would need to go in here - move to getEulerBOLD
 
 # Reproduce Fig. 4
 # stimulus = 30
@@ -47,7 +41,6 @@ def euler_1st(func, t, h, var_init, indicator,  alpha, beta, select=False, add_n
     # Initial values
     (xE_val, xI_val, a_val, f_val, v_val, q_val, fout_val, E_val, y_val
      ) = var_init
-
     # List for plotting
     t_list = [t[0]]  # Ourput list if select=True
     u_list = []
@@ -59,22 +52,22 @@ def euler_1st(func, t, h, var_init, indicator,  alpha, beta, select=False, add_n
     q_list = [q_val]
     E_list = [E_val]
     fout_list = [fout_val]
-    y_list = [y_val]
+    y_list = [torch.tensor(y_val, requires_grad=True)]
     if add_noise:
         noise, _ = generate_pink_noise(len(t), t[-1], alpha, beta) #check
     for i in range(0, len(t)-1):
         # Stimulus
         u_val = func.sti_u(t[i])
 
+
         # ODEs
-        xE_val_next = xE_val + h*func.ode_xE(u=u_val, xE=xE_val, xI=xI_val) + (np.sqrt(h) * noise[i] if add_noise else 0)
+        xE_val_next = xE_val + h*func.ode_xE(u=u_val, xE=xE_val, xI=xI_val) + (math.sqrt(h) * noise[i] if add_noise else 0)
         xI_val_next = xI_val + h*func.ode_xI(xE=xE_val, xI=xI_val)
         a_val_next = a_val + h*func.ode_a(a=a_val, xE=xE_val)
         f_val_next = f_val + h*func.ode_f(a=a_val, f=f_val)
         v_val_next = v_val + h*func.ode_v(f=f_val, fout=fout_val)
         q_val_next = q_val + h*func.ode_q(f=f_val, E=E_val, fout=fout_val,
                                           q=q_val, v=v_val)
-
         # Non-ODEs
         if indicator is True:
             fout_val_next = func.equ_fout(v=v_val_next, couple=True)
@@ -83,7 +76,6 @@ def euler_1st(func, t, h, var_init, indicator,  alpha, beta, select=False, add_n
                                           couple=False)
         E_val_next = func.equ_E(f=f_val_next)
         y_val_next = func.equ_y(q=q_val_next, v=v_val_next)
-
         # Append results
         if select is False:
             u_list.append(u_val)
@@ -115,31 +107,35 @@ def euler_1st(func, t, h, var_init, indicator,  alpha, beta, select=False, add_n
         E_val = E_val_next
 
     u_list.append(func.sti_u(t[-1]))
-
     if select is False:
-        u_list = np.array(u_list)
-        xE_list = np.array(xE_list)
-        xI_list = np.array(xI_list)
-        a_list = np.array(a_list)
-        f_list = np.array(f_list)
-        v_list = np.array(v_list)
-        q_list = np.array(q_list)
-        fout_list = np.array(fout_list)
-        E_list = np.array(E_list)
-        y_list = np.array(y_list)
-
+        u_list = torch.tensor(u_list)
+        xE_list = torch.tensor(xE_list)
+        xI_list = torch.tensor(xI_list)
+        a_list = torch.tensor(a_list)
+        f_list = torch.tensor(f_list)
+        v_list = torch.tensor(v_list)
+        q_list = torch.tensor(q_list)
+        fout_list = torch.tensor(fout_list)
+        E_list = torch.tensor(E_list)
+        # y_list = torch.tensor(y_list, requires_grad=True)
+        # print("u_val", torch.autograd.grad(y_val_next, func.getMTT(), retain_graph=True))
         return (u_list, xE_list, xI_list, a_list, f_list, v_list, q_list,
                 fout_list, E_list, y_list)
     else:
         return t_list, y_list
 
 
-def getEulerBOLD(alpha=None, beta=None, noise=False):
-    # Iteration values
-    t_ref = spb.time_ref(stimulus)  # controls initial and final t
+def getEulerBOLD(mtt, alpha=None, beta=None, noise=False, length=None):
 
+    # Iteration values
+    func.setMTT(mtt)  # Use the tensor instead of the scalar mtt
+
+    t_ref = spb.time_ref(stimulus)  # controls initial and final t
     h = 0.01  # step size, unit: sec
-    t = np.arange(t_ref[0], t_ref[-1] + t_ref[1], h)  # all t values
+    if length == None:
+        t = torch.arange(t_ref[0], t_ref[-1] + t_ref[1], h)  # all t values
+    else:
+        t = torch.arange(t_ref[0], length + t_ref[1], h)
 
     # Initial conditions
     var_init = [spb.xE_init, spb.xI_init, spb.a_init, spb.f_init, spb.v_init,
@@ -149,12 +145,7 @@ def getEulerBOLD(alpha=None, beta=None, noise=False):
     (u_list, xE_list, xI_list, a_list, f_list, v_list, q_list, fout_list,
      E_list, y_list) = euler_1st(func, t, h, var_init, indicator, alpha, beta, False, noise)
 
-    plt.plot(t, y_list)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Magnitude')
-    plt.title('P-DCM HRF')
-    plt.show()
-    print(y_list)
+    # Return the tensors you want to compute gradients with respect to
     return t, y_list
 
-getEulerBOLD(10, 1, True)
+#getEulerBOLD(3, 1, True, None)

NoiseTimeSeries.py
@@ -2,9 +2,9 @@ import torch
 import torch.fft as fft
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 # Determine the number of points, n, and the length, T
-def generate_pink_noise(n, T, alpha, beta):
+def generate_pink_noise(n, T, alpha, beta, scaling_factor=0.005):
     """"This also determines the spectral space with wave numbers -k_max to k_max which correspond to frequency values f_k=kT/(2𝜋)
         .
         
@@ -27,6 +27,7 @@ def generate_pink_noise(n, T, alpha, beta):
 
     #Set the magnitudes of your spectral coefficients: C_k=1/|f_k|
     C_k = alpha *  f_k ** (-1 * beta)#plot csd of data against this in semi y log plot -> should be alpha beta, check that real and sampled match for different values
+
     #Set C_0=0 to give zero mean to the noise sequence.
     C_k[0] = 0  # Zero mean
 
@@ -55,7 +56,7 @@ def generate_pink_noise(n, T, alpha, beta):
     # Normalize noise to have unit variance - optional addition not in orignal algo?
     noise_time_domain /= torch.std(noise_time_domain)
 
-    return noise_time_domain, C_k_complex
+    return noise_time_domain * scaling_factor, C_k_complex
 
 def plot_noise_time_domain(noise_sequence, T):
     plt.figure(figsize=(10, 5))

Stimulus_PDCM_BOLD.py
@@ -1,19 +1,4 @@
-"""
-Functions for the stimulus and the equations from P-DCM model
-
-Default constant values
-- Values are from Tables 1A and 1B (values for 3T field strength and GE
-                                    acquisition method)
-- TE = 35/1000, which is stated in Method section
-
-The list of time values for error analysis is also included
-
-References:
-    Journal article: https://doi.org/10.1016/j.neuroimage.2015.07.078
-"""
-
-import numpy as np
-
+import torch
 
 class StimulusPDCMBOLD:
     """Generate the stimulus and the P-DCM equations and ODEs to calculate the
@@ -23,7 +8,7 @@ class StimulusPDCMBOLD:
     - All constants involved in the equations and ODEs (details and default
       values see below)
     - ignore_range -- Ignore plausible range checks
-    - cross_valid -- Set to True when running CrossValidation.py becuase mu
+    - cross_valid -- Set to True when running CrossValidation.py because mu
                      was outside the plausible range for the plots generated
                      in Havlicek et. al.'s paper
     """
@@ -31,7 +16,7 @@ class StimulusPDCMBOLD:
     def __init__(self, w=1.0,
                  sigma=0.5, mu=0.4, lamb=0.2, c=0.25,
                  psi=0.6, phi=1.5, chi=0.6,
-                 tMTT=2.0, tau=4.0, alpha=0.32, E0=0.4,
+                 tMTT=torch.tensor(2.0, requires_grad=True), tau=4.0, alpha=0.32, E0=0.4,
                  V0=4,
                  epsilon=0.3, theta0=80.6, r0=108.0, TE=35.0,
                  ignore_range=False,
@@ -82,6 +67,7 @@ class StimulusPDCMBOLD:
         self._r0 = r0  # sensitivity
         self._TE = TE/1000  # echo time, ms, 30-45
 
+
     # Stimulus
     def sti_u(self, t):
         """Function of the input stimulus, u(t), which
@@ -89,7 +75,7 @@ class StimulusPDCMBOLD:
         - is a rectangular function starting at t = 1s with width, w, by
           default
         """
-        return np.where(t >= 1 and t <= (self._w + 1), 1, 0)
+        return torch.where((t >= 1) & (t <= (self._w + 1)), torch.tensor(1.0), torch.tensor(0.0))
 
     # Equations and ODEs in P-DCM
     def ode_xE(self, u, xE, xI):
@@ -148,6 +134,7 @@ class StimulusPDCMBOLD:
         - q -- deoxyhemoglobin content
         - v -- blod volume
         """
+        # print(self._tMTT)
         return (f*(E/self._E0) - fout*(q/v))/self._tMTT
 
     def equ_E(self, f):
@@ -166,6 +153,7 @@ class StimulusPDCMBOLD:
         - f -- blood inflow response
         - couple -- is True when using CBF-CBV coupled model (i.e. tau = 0)
         """
+        # print(self._tMTT)
         if couple is True:
             return v**(1/self._alpha)
         return (1/(self._tau + self._tMTT)) * (
@@ -186,6 +174,12 @@ class StimulusPDCMBOLD:
 
         return self._V0 * (k1*(1 - q) + k2*(1 - q/v) + k3*(1 - v))
 
+
+    def setMTT(self, mtt):
+        self._tMTT = mtt
+
+    def getMTT(self):
+        return self._tMTT
 
 def time_ref(stimulus, analysis=False):
     """Produce a reference list of time values for iterations in numerical
@@ -214,7 +208,7 @@ def time_ref(stimulus, analysis=False):
     else:
         tf = 150
 
-    return np.arange(ti, tf, h)  # all t values
+    return torch.arange(ti, tf, h)  # all t values
 
 
 def time_select(t_ref, t_all, y_all):
@@ -231,7 +225,7 @@ def time_select(t_ref, t_all, y_all):
     for i in range(len(t_all)):
         if k < len(t_ref):
             # append when the item in t = the item in t_ref
-            if t_ref[k] - t_all[i] <= 1e-15:
+            if torch.abs(t_ref[k] - t_all[i]) <= 1e-15:
                 y_select.append(y_all[i])
                 k += 1
 