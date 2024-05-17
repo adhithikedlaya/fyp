32428693a1b56a430a1fd91a6b2f1e2e62dddcce
ak1920 <ak1920@ic.ac.uk>
2024-05-17 17:48:50 +0100

    remove array job

--- pytorch models/single_subject_script.sh
+++ pytorch models/single_subject_script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=4:mem=24gb:ngpus=1:gpu_type=RTX6000
-#PBS -l walltime=01:00:00
+#PBS -l walltime=48:00:00
 #PBS -N p-dcm_fitting
-#PBS -J 0-5
 
 
 module load anaconda3/personal
 source activate pytorch_env
 cd $PBS_O_WORKDIR
-num_regions = 116
-subj = $(($PBS_ARRAY_INDEX + 1/ num_regions)) 
-roi = $(($PBS_ARRAY_INDEX % num_regions))
-exp = "PLCB"
-python3 time_domain_model $subj $roi $exp > /rds/general/user/ak1920/home/fyp/fyp/pytorch models/output_${exp}_${subj}_${roi}.txt
+
+for ((subj=1; subj<3; subj+=1)); do
+    for ((roi=0; roi<116; roi+=step)); do
+        exp = "PLCB"
+        python3 time_domain_model $subj $roi $exp > /rds/general/user/ak1920/home/fyp/fyp/pytorch models/output_${exp}_${subj}_${roi}.txt
+    done
+done
+
 
 
